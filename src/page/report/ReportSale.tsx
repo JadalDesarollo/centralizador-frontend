d04825636a9bc1a16672dca2dcb8d031904d4c64
Renzo O <renzo.otrejo@jadalfe.pe>
2023-10-24 11:44:15 -0500

    fix:style pdf

--- src/page/report/ReportSale.tsx
+++ src/page/report/ReportSale.tsx
@@ -31,27 +31,38 @@ function ReportSale() {
 
   return (
     <Card padding="1.4rem">
+      <Box px={20}>
+        <Text heading="h5" styles={{ minWidth: 120 }}>
+          Imprimir desde las fechas
+        </Text>
+        <Box display="flex" align="center" mb={16}>
+          <Text heading="h6" styles={{ minWidth: 120 }}>
+            Desde:
+          </Text>
+          <TextField
+            type="text"
+            name="invoice-date_issue"
+            sizes="small"
+            maxWidth="250px"
+            defaultValue="2023-10-01" // Establece el valor por defecto
+          />
+        </Box>
+        <Box display="flex" align="center" mb={16}>
+          <Text heading="h6" styles={{ minWidth: 120 }}>
+            Hasta:
+          </Text>
+          <TextField
+            type="text"
+            name="invoice-date_due"
+            sizes="small"
+            maxWidth="250px"
+            defaultValue="2023-10-31" // Establece el valor por defecto
+          />
+        </Box>
     <div>
-     <div>
-        Desde:
-        <input
-          type="text"
-          name="invoice-date_issue"
-          defaultValue="2023-10-01"
-        />
-      </div>
-      <div>
-        Hasta:
-        <input
-          type="text"
-          name="invoice-date_due"
-          defaultValue="2023-10-31"
-        />
-      </div>
       <Button onClick={downloadAndCachePDF}>Visualizar PDF</Button>
       {/* <button onClick={openPDFViewer}>Visualizar PDF</button> */}
       <br />
-      <p>ssssssssssss</p>
       <div>
         {pdfUrlCached ? (
           <iframe src={pdfUrlCached} width="100%" height="600"></iframe>
@@ -61,6 +72,7 @@ function ReportSale() {
       
     </div>
     </div>
+      </Box>
     </Card>
   );
 }
